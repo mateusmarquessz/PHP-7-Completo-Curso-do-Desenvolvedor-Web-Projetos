a2774b4b1f0e58ccc285f2d22f88fbe0ec33a621
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-02-03 10:53:35 -0300

    att

---
+++ EXTRA JavaScript/Fundamentos/01-organizacao.js
@@ -0,0 +1,8 @@
+console.log("Sentenca de codigo") //console.log mostra na tela, isto e uma sentenca de codigo
+
+{ //Blocos de codigos, UM agrupador de sentecas de codigo
+    {
+        console.log("Ola")
+        console.log("Mundo!")
+    }
+}
---
+++ EXTRA JavaScript/Fundamentos/02-Comentarios.js
@@ -0,0 +1,16 @@
+// Comentario de uma linha
+console.log("Linha 1")
+
+
+/* 
+Comentarios de
+Muitiplas Linhas
+*/
+
+console.log("Linha 2")
+
+/*
+Comentarios de multiplas Linhas
+*/
+
+console.log("Linhas 3")
