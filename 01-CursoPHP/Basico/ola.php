98b9af564ed3bcb6db2865d1818fa69adc2e986c
Mateus Marques <81566403+mateusmarquessz@users.noreply.github.com>
2023-04-08 12:18:51 -0300

    att

--- 01-CursoPHP/Basico/ola.php
+++ 01-CursoPHP/Basico/ola.php
@@ -1,1 +1,13 @@
 <div class="titulo">Ola PHP</div>
+
+<h2>Exemplo A </h2>
+<?php 
+echo 'Ola ';
+echo 'Mundo!';
+?>
+
+<h2>Exemplo B </h2>
+<?= "Outra formade me 'expressar' !"?>
+
+<?php
+phpinfo();
